fc8ac3af5fa6d55ce549b8466a8764f1b2374342
Metalluga <semrog@yandex.ru>
2016-04-23 17:24:49 +0300

    Update array.c

--- array.c
+++ array.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define ARRAY_SIZE 1000
 
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    if(argc != 3)
     {
         printf("Wrong number of command line arguments\n");
         return 1;
     }
     FILE *fp = fopen(argv[1] , "a+");
     if(!fp)
-    {   
+    {
         printf("Cannot open file\n");
         return -1;
     }
+    int ARRAY_SIZE = atoi(argv[2]);
     long start = clock();
-    static double arr[ARRAY_SIZE][ARRAY_SIZE];
+    double arr[ARRAY_SIZE][ARRAY_SIZE];
     for(int i = 0; i < ARRAY_SIZE; i ++)
     {
         for(int j = 0; j < ARRAY_SIZE; j ++)
